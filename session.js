a5226adb6123e68ca8d6ebf3196beae727308d09
pathumkalhara <kalharapathum831@gmail.com>
2024-12-05 05:27:08 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-MD=iRU1zKIJ#cVMMaoGTgqZzGWSX_RUsEmxM4Ksduptoid6Bf9gJwdo",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=qYclELDD#7uL7pyz3juimzBeJCmLMGHESXt4kiIQ0AZ5Teim6-E0",
 };
